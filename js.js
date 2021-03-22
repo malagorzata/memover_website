1122136271b3fcee89cca336a0d922118454b8e6
malagorzata <gosia.warwaszynska@gmail.com>
2021-03-22 19:46:55 +0100

    changes to buttons

--- js.js
+++ js.js
@@ -6,8 +6,8 @@ let stopBtn = document.querySelector("#stopBtn");
 let intro = document.querySelector("#intro");
 
 function start() {
-  playBtn.style.backgroundImage = "url(play.svg)";
-  stopBtn.style.backgroundImage = "url(stop.svg)";
+  //   playBtn.style.backgroundImage = "url(play.svg)";
+  //   stopBtn.style.backgroundImage = "url(stop.svg)";
 
   playBtn.addEventListener("click", playVideo);
   stopBtn.addEventListener("click", stopVideo);
@@ -18,13 +18,13 @@ function playVideo() {
   console.log("playVideo");
   if (videoScr.paused == true) {
     videoScr.play();
-    playBtn.style.backgroundImage = "url(stop.svg)";
+    // playBtn.style.backgroundImage = "url(stop.svg)";
     console.log(videoScr.paused);
     document.querySelector("#playBtn").classList.add("hidden");
     document.querySelector("#intro").classList.add("hidden");
   } else {
     videoScr.pause();
-    playBtn.style.backgroundImage = "url(play.svg)";
+    // playBtn.style.backgroundImage = "url(play.svg)";
   }
 }
 
@@ -35,7 +35,7 @@ function stopVideo() {
   document.querySelector("#intro").classList.remove("hidden");
 
   videoScr.currentTime = 0;
-  playBtn.style.backgroundImage = "url(play.svg)";
+  //   playBtn.style.backgroundImage = "url(play.svg)";
 }
 
 $(function () {
