7b25e92129774d70f1e0527e29e6db63296a0afa
malagorzata <gosia.warwaszynska@gmail.com>
2021-03-23 08:00:27 +0100

    added css headings and created img folder

--- js.js
+++ js.js
@@ -6,9 +6,6 @@ let stopBtn = document.querySelector("#stopBtn");
 let intro = document.querySelector("#intro");
 
 function start() {
-  //   playBtn.style.backgroundImage = "url(play.svg)";
-  //   stopBtn.style.backgroundImage = "url(stop.svg)";
-
   playBtn.addEventListener("click", playVideo);
   stopBtn.addEventListener("click", stopVideo);
   document.querySelector("#intro").classList.remove("hidden");
@@ -18,13 +15,11 @@ function playVideo() {
   console.log("playVideo");
   if (videoScr.paused == true) {
     videoScr.play();
-    // playBtn.style.backgroundImage = "url(stop.svg)";
     console.log(videoScr.paused);
     document.querySelector("#playBtn").classList.add("hidden");
     document.querySelector("#intro").classList.add("hidden");
   } else {
     videoScr.pause();
-    // playBtn.style.backgroundImage = "url(play.svg)";
   }
 }
 
@@ -35,5 +30,4 @@ function stopVideo() {
   document.querySelector("#intro").classList.remove("hidden");
 
   videoScr.currentTime = 0;
-  //   playBtn.style.backgroundImage = "url(play.svg)";
 }
